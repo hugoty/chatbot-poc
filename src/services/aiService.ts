00836cc2026f44e3d11ee2cf4ccac901f09ee095
AmeniLefi <ameni.lefi@esprit.tn>
2024-05-24 16:02:50 +0200

    API jira

--- src/services/aiService.ts
+++ src/services/aiService.ts
@@ -1,4 +1,4 @@
-import { AnthropicClient } from "../clients/anthropicClient";
+import { AnthropicClient } from "../clients/AnthropicClient";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
 import { searchInDB } from "./dbService";  // Méthode renommée
 import { StringOutputParser } from "@langchain/core/output_parsers";
@@ -61,4 +61,67 @@ export class AIService {
       throw error;
     }
   }
+
+  async classifyRequest(question: string): Promise<any> {
+    const prompt = ChatPromptTemplate.fromTemplate(`
+      Classify the following text as either "demande ticket jira" , "demande d'action" or "demande d'information".
+
+        Text : {input}
+
+        Class :
+    `);
+
+    const parser = new StringOutputParser();
+
+    try {
+      console.log("go");
+      const chain = prompt.pipe(AnthropicClient);
+
+      const response = await chain.invoke({
+        input: question,
+      });
+
+      return response;
+    } catch (error) {
+      console.error("Error invoking AI model:", error);
+      throw error;
+    }
+  }
+  
+  async detectTaskTypeAndGenerateDescription(input: string): Promise<{ taskType: string, description: string }> {
+    const prompt = `
+        You are an assistant that helps classify tasks and generate descriptions for Jira issues.
+        Based on the following input, determine the task type and provide a detailed description.
+
+        Input: {input}
+
+        Output format:
+        Task Type: <Task Type>
+        Description: <Description>
+    `;
+
+    try {
+        const chain = ChatPromptTemplate.fromTemplate(prompt).pipe(AnthropicClient);
+
+        const response = await chain.invoke({
+            input: input,
+        });
+
+        const content = response.content.toString();
+        const lines = content.split('\n');
+
+        const taskTypeLine = lines[0].trim();
+        const descriptionLine = lines[1].trim();
+
+        const taskType = taskTypeLine.split(': ')[1].trim();
+        const description = descriptionLine.split(': ')[1].trim();
+
+        return { taskType, description };
+    } catch (error) {
+        console.error('Error invoking AI model:', error);
+        throw error;
+    }
+}
+
+
 }
